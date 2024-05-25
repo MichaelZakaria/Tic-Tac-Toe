6c7a5035445da5464aaa55c8ddaef85d7249358d
MichaelZakaria <michaelzakaria39@gmail.com>
2024-05-25 13:33:11 +0300

    Limited orientation to portrait only, modified app label and fixed font issues

--- lib/common/fonts/my_fonts.dart
+++ lib/common/fonts/my_fonts.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyFonts {
-  static var myFontBlack = GoogleFonts.pressStart2p(
-      textStyle: const TextStyle(color: Colors.black, letterSpacing: 2, fontSize: 15)
-  );
-  static var myFontWhite = GoogleFonts.pressStart2p(
-      textStyle: const TextStyle(color: Colors.white, letterSpacing: 2, fontSize: 15)
-  );
+  static var myFontBlack = const TextStyle(fontFamily: "PressStart2P", color: Colors.black, letterSpacing: 2, fontSize: 15);
+  static var myFontWhite = const TextStyle(fontFamily: "PressStart2P", color: Colors.white, letterSpacing: 2, fontSize: 15);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'views/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(const MyApp());
 }
 
